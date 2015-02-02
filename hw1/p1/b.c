e7e43110e90eb04f4f804151bde20b4aefb5c9f2
johannes <eson517@gmail.com>
2015-02-02 18:54:30 +0100

    p1 och p6 palyndrome klara

---
+++ hw1/p1/b.c
@@ -0,0 +1,141 @@
+/* matrix summation using pthreads
+
+   features: uses a barrier; the Worker[0] computes
+             the total sum from partial sums computed by Workers
+             and prints the total sum to the standard output
+
+   usage under Linux:
+     gcc matrixSum.c -lpthread
+     a.out size numWorkers
+
+*/
+#ifndef _REENTRANT 
+#define _REENTRANT 
+#endif 
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+#define MAXSIZE 4  /* maximum matrix size */
+#define MAXWORKERS 2   /* maximum number of workers */
+#define DEBUG yes
+
+
+int numWorkers;           /* number of workers */ 
+
+
+
+
+/* timer */
+double read_timer() {
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized )
+    {
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+double start_time, end_time; /* start and end times */
+int size, stripSize;  /* assume size is multiple of numWorkers */
+int sums[MAXWORKERS]; /* partial sums */
+int matrix[MAXSIZE][MAXSIZE]; /* matrix */
+
+int sum = 0;
+pthread_mutex_t sum_lock;
+
+void increment_sum(int n) {
+  pthread_mutex_lock(&sum_lock);
+  sum+=n;
+  pthread_mutex_unlock(&sum_lock);
+}
+
+void *Worker(void *);
+
+
+/* read command line, initialize, and create threads */
+int main(int argc, char *argv[]) {
+  int i, j;
+  long l; /* use long in case of a 64-bit system */
+  pthread_attr_t attr;
+  pthread_t workerid[MAXWORKERS];
+
+  /* set global thread attributes */
+  pthread_attr_init(&attr);
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+  /* initialize mutex and condition variable */
+  pthread_mutex_init(&sum_lock, NULL);
+
+  /* read command line args if any */
+  size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
+  numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
+  if (size > MAXSIZE) size = MAXSIZE;
+  if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
+  stripSize = size/numWorkers;
+
+  /* initialize the matrix */
+  for (i = 0; i < size; i++) {
+	  for (j = 0; j < size; j++) {
+          matrix[i][j] = 1;//rand()%99;
+	  }
+  }
+
+  /* print the matrix */
+#ifdef DEBUG
+  for (i = 0; i < size; i++) {
+	  printf("[ ");
+	  for (j = 0; j < size; j++) {
+	    printf(" %d", matrix[i][j]);
+	  }
+	  printf(" ]\n");
+  }
+#endif
+
+  /* do the parallel work: create the workers */
+  start_time = read_timer();
+  for (l = 0; l < numWorkers; l++)
+    pthread_create(&workerid[l], &attr, Worker, (void *) l);
+  //pthread_exit(NULL);
+  for (l = 0; l < numWorkers; l++)
+       pthread_join(workerid[l], NULL);
+
+  end_time = read_timer();
+
+  printf("The total is %d\n", sum);
+  printf("The execution time is %g sec\n", end_time - start_time);
+  return 0;
+}
+
+/* Each worker sums the values in one strip of the matrix.
+   After a barrier, worker(0) computes and prints the total */
+
+void increment_sum(int);
+
+void *Worker(void *arg) {
+  long myid = (long) arg;
+  int total, i, j, first, last;
+  int my_row;
+
+#ifdef DEBUG
+  printf("worker %d (pthread id %d) has started\n", myid, pthread_self());
+#endif
+
+
+  /* determine first and last rows of my strip */
+  first = myid*stripSize;
+  last = (myid == numWorkers - 1) ? (size - 1) : (first + stripSize - 1);
+
+  /* sum values in my strip */
+  total = 0;
+  for (i = first; i <= last; i++)
+    for (j = 0; j < size; j++)
+      total += matrix[i][j];
+  increment_sum(total);
+}
---
+++ hw1/p6/palindrome.c
@@ -0,0 +1,212 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+
+#define MAXWORKERS 8
+#ifndef WORKERS
+	#define WORKERS MAXWORKERS
+#endif
+#define WORDS 26000
+#define STRIP 10
+
+int words[WORDS], numWords, palindromesOffset = 0, totalPalindromes = 0;
+char *buffer;
+FILE *of;
+double start_time, end_time;
+
+char *strrev(char *str)
+{
+      char *p1, *p2;
+
+      if (! str || ! *str)
+            return str;
+      for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+      {
+            *p1 ^= *p2;
+            *p2 ^= *p1;
+            *p1 ^= *p2;
+      }
+      return str;
+}
+
+/* timer */
+double read_timer() {
+    static bool initialized = false;
+    static struct timeval start;
+    struct timeval end;
+    if( !initialized )
+    {
+        gettimeofday( &start, NULL );
+        initialized = true;
+    }
+    gettimeofday( &end, NULL );
+    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+
+pthread_mutex_t pali_lock;
+void write_palindromes(const char *p) {
+	pthread_mutex_lock(&pali_lock);
+	fprintf(of, "%s", (const char *)p);
+	pthread_mutex_unlock(&pali_lock);
+}
+
+pthread_mutex_t tot_pali;
+void incr_pali() {
+	pthread_mutex_lock(&tot_pali);
+	totalPalindromes++;
+	pthread_mutex_unlock(&tot_pali);
+}
+
+pthread_mutex_t bag_lock;
+int current_word = 0;
+void bag(int *start, int *end) {
+	pthread_mutex_lock(&bag_lock);
+	if(current_word==numWords) *start = -1;
+	else {
+		*start = current_word;
+		*end = (current_word + STRIP -1 > numWords -1) ? numWords -1 : current_word + STRIP -1;
+		current_word = *end + 1;
+	}
+	pthread_mutex_unlock(&bag_lock);
+	
+}
+
+int find_word(const char *w) {
+	int i, cmp;
+
+	for(i = 0; i < numWords; i++) {
+		cmp = strcasecmp(w, buffer + words[i]);
+		if(!cmp) return i;
+	}
+	return -1;
+}
+
+void bag(int*, int*);
+int find_word(const char*);
+char *strrev(char *);
+void write_palindromes(const char*);
+void incr_pali(void);
+
+
+void worker(void *arg) {
+	int start, end, match, numPalindromes, offset;
+	long myid = (long) arg;
+	char buf[30];
+	char res[5000];
+
+
+	while(1) {
+		bag(&start,&end);
+		
+		if(start==-1) break;
+
+		while(start<=end) {
+			strcpy(buf, buffer + words[start]);	
+			strrev(buf);
+			if(find_word(buf)>-1) {
+				incr_pali();
+				numPalindromes++;
+				strcpy(res + offset, buffer + words[start]);
+				offset+=strlen(buffer + words[start]);
+				res[offset] = '\n';
+				offset++;
+			}
+			start++;
+		}
+
+	}
+	res[offset]='\0';
+	write_palindromes((const char *) &res);
+	printf("Thread with ID %lu found %d palindromes\n", myid, numPalindromes);
+}
+
+int main(int argc, char *argv[]) {
+	
+	FILE *file;
+	long numbytes;
+	int index, i, wordstart, numWorkers;
+	long l; /* use long in case of a 64-bit system */
+ 	pthread_attr_t attr;
+  	pthread_t workerid[MAXWORKERS];
+
+	file = fopen("words","r");
+	of = fopen("output", "w");
+
+	if(!file) {
+		fprintf(stderr, "no such file\n");
+		exit(1);
+	}
+
+	fseek(file, 0L, SEEK_END);
+	numbytes = ftell(file);
+
+/* reset the file position indicator to 
+the beginning of the file */
+	fseek(file, 0L, SEEK_SET);	
+
+/* grab sufficient memory for the 
+buffer to hold the text */
+	buffer = (char*)calloc(numbytes, sizeof(char));	
+
+/* memory error */
+	if(buffer == NULL) {
+		fprintf(stderr, "could not allocate memory for buffer\n");
+		exit(1);
+	}
+
+/* copy all the text into the buffer */
+	fread(buffer, sizeof(char), numbytes, file);
+	fclose(file);
+
+/* confirm we have read the file by
+outputing it to the console */
+	//printf("The file called test.dat contains this text\n\n%s", buffer);
+
+	numWords=0;
+	wordstart=0;
+	for(i = 0; i < numbytes; i++) {
+		//buffer[i] = tolower(buffer[i]);
+		if(buffer[i]=='\n') {
+			buffer[i]='\0';
+			words[numWords++] = wordstart;
+			wordstart = i+1;
+		}
+	}
+
+	numWorkers = WORKERS > MAXWORKERS ? MAXWORKERS : WORKERS;
+	pthread_attr_init(&attr);
+  	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+
+  	pthread_mutex_init(&bag_lock, NULL);
+  	pthread_mutex_init(&pali_lock, NULL);
+  	pthread_mutex_init(&tot_pali, NULL);
+
+  	start_time = read_timer();
+  	for (l = 0; l < numWorkers; l++)
+    	pthread_create(&workerid[l], &attr, (void *)&worker, (void *) l);
+    for (l = 0; l < numWorkers; l++)
+       pthread_join(workerid[l], NULL);
+   end_time = read_timer();
+   printf("Total palindromes found: %d\n",totalPalindromes);
+    printf("The execution time was %g sec\n", end_time - start_time);
+   fclose(file);
+   fclose(of);
+
+
+
+
+
+
+/* free the memory we used for the buffer */
+	free(buffer);
+
+
+}
